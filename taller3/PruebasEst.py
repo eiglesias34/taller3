9fec4cb232d7d56435a9894c5264699b610538e0
Enrique <eiglesias34@gmail.com>
2015-01-29 16:53:45 -0430

    Paso 5. Caso de prueba falla.

--- taller3/PruebasEst.py
+++ taller3/PruebasEst.py
@@ -48,11 +48,9 @@ class TestEst(unittest.TestCase):
         horaFin = datetime.strptime("18:00", formato)
         self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
         
-        #Esquina: Ingresar reservacion hora minima despues de varias reservaciones
-        #         que terminen en hora maxima (Falla)
-        reservas =[[6,-1],[7,-1],[8,-1],[9,-1],[10,-1],[11,-1],[12,-1],[13,-1],[14,-1],[15,-1],
-                   [9,1],[10,1],[11,1],[12,1],[13,1],[14,1],[15,1],[16,1],[17,1],[18,1]]
-        horaIni = datetime.strptime("06:00", formato)
-        horaFin = datetime.strptime("09:00", formato)
+        #Malicia: Comienzan unas reservas a las 13:00 y otra termina cuando empiezan estas. 
+        reservas =[[12,-1],[12,-1],[12,-1],[12,-1],[12,-1],[13,-1],[13,-1],[13,-1],[13,-1],[13,-1],
+                   [14,1],[14,1],[14,1],[14,1],[14,1],[15,1],[15,1],[15,1],[15,1],[15,1]]
+        horaIni = datetime.strptime("10:00", formato)
+        horaFin = datetime.strptime("13:00", formato)
         self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), True)
-        
