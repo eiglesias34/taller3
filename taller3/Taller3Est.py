9c50bc80ab382e51655a68b67ca61bd558d8c9c9
Enrique <eiglesias34@gmail.com>
2015-01-29 15:24:02 -0430

    Paso2. Construyendo

--- taller3/Taller3Est.py
+++ taller3/Taller3Est.py
@@ -23,6 +23,45 @@ class Estacionamiento:
     
 if __name__ == '__main__':
     est = Estacionamiento()
-    reservas = []
     formato = '%H:%M'
+    reservas = []
     
+    while True:    
+        while True:
+            horaIni = raw_input("Introduzca hora del inicio de la reservacion (hh:mm): ")
+            horaIni = datetime.strptime(str(horaIni), formato)
+            if ((horaIni.minute == 0) and (est.getHoraMinReserva() <= horaIni.hour < est.getHoraMaxReserva())):
+                break;
+            else:
+                print("Hora invalida.")
+
+        while True:
+            hf = raw_input("Introduzca hora de la finalizacion de la reservacion (hh:mm): ")
+            hf = datetime.strptime(str(hf), formato)
+            if ((hf.minute == 0) and (est.getHoraMinReserva() < hf.hour <= est.getHoraMaxReserva()) and (hf.hour > horaIni.hour)):
+                break;
+            else:
+                print("Hora invalida.")
+                
+        if len(est.reservaciones) == 0:
+            est.reservaciones.append([horaIni.hour,hf.hour])
+            print("Se realizo una reservacion exitosa")
+        else:
+            if reservas != []: 
+                if (est.verificarDisponibilidad(reservas, horaIni, hf)):
+                    print("Se realizo una reservacion exitosa")
+                else:
+                    print("No se puede realizar la reserva solicitada")
+            else:
+                est.reservaciones.append([horaIni.hour,hf.hour])
+                print("Se realizo una reservacion exitosa")
+
+        while True:
+            continuar = raw_input("Desea hacer otra reservacion? (si/no): ")
+            if (continuar != "si") and (continuar != "no"):
+                print("Valor invalido.")
+            else:
+                break
+
+        if continuar == "no":
+            break
