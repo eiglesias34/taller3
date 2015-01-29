990409face21bd322d8b3dec5bf38b6e9848cf39
Enrique <eiglesias34@gmail.com>
2015-01-29 18:32:54 -0430

    Paso 10, Refactorizando. Haciendo pruebas de regresion

--- taller3/PruebasEst.py
+++ taller3/PruebasEst.py
@@ -10,66 +10,8 @@ class TestEst(unittest.TestCase):
         est = Estacionamiento()
         
         #Frontera: Primera reservacion
-        reservas = [[6,-1],[9,1]]
+        est.reservaciones = [[6,9]]    
         horaIni = datetime.strptime("06:00", formato)
         horaFin = datetime.strptime("09:00", formato)
-        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), True)
+        self.assertEqual(est.verificarDisponibilidad(horaIni.hour,horaFin.hour), True)
         
-        #Frontera: Maximo de reservas posibles en un intervalo
-        reservas =[ [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], 
-                [9,1], [9,1], [9,1], [9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1]]
-        horaIni = datetime.strptime("06:00", formato)
-        horaFin = datetime.strptime("09:00", formato)
-        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
-        
-        #Esquina: Tratar de realizar una reserva donde el intervalo esta lleno
-        reservas =[[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],
-                   [9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],
-                   [9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],
-                   [10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1]]
-        horaIni = datetime.strptime("07:00", formato)
-        horaFin = datetime.strptime("18:00", formato)
-        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
-        
-        #Esquina: Tratar de realizar una reserva donde el intervalo esta lleno
-        reservas =[[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],
-                   [9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],
-                   [9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],
-                   [10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1]]
-        horaIni = datetime.strptime("08:00", formato)
-        horaFin = datetime.strptime("10:00", formato)
-        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
-
-        #Frontera: Tratar de realizar una reserva teniendo el estacionamiento
-        #          lleno.
-        reservas = [[6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1],
-                    [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1]]
-        horaIni = datetime.strptime("06:00", formato)
-        horaFin = datetime.strptime("18:00", formato)
-        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
-        
-        #Malicia: Comienzan unas reservas a las 13:00 y una nueva termina cuando empiezan estas. 
-        reservas =[[12,-1],[12,-1],[12,-1],[12,-1],[12,-1],[13,-1],[13,-1],[13,-1],[13,-1],[13,-1],
-                   [14,1],[14,1],[14,1],[14,1],[14,1],[15,1],[15,1],[15,1],[15,1],[15,1]]
-        horaIni = datetime.strptime("10:00", formato)
-        horaFin = datetime.strptime("13:00", formato)
-        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), True)
-        
-        #Esquina: Ingresar reservacion hora minima despues de varias reservaciones
-        #         que terminen en hora maxima
-        reservas =[[6,-1],[7,-1],[8,-1],[9,-1],[10,-1],[11,-1],[12,-1],[13,-1],[14,-1],[15,-1],
-                   [9,1],[10,1],[11,1],[12,1],[13,1],[14,1],[15,1],[16,1],[17,1],[18,1]]
-        horaIni = datetime.strptime("06:00", formato)
-        horaFin = datetime.strptime("09:00", formato)
-        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), True)
-        
-        #Esquina, Frontera y Malicia: 10 carros reservan por la mitad del dia (06:00 - 12:00) y otros 
-        #                            10 reservan por el resto del dia (12:00 - 18:00); luego otro carro trata
-        #                            de reservar por una hora de 11:00 a 12:00. (FALLA)
-        reservas = [[6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1],
-                    [12,-1], [12,-1], [12,-1], [12,-1], [12,-1], [12,-1], [12,-1], [12,-1], [12,-1],
-                    [12,1], [12,1], [12,1], [12,1], [12,1], [12,1], [12,1], [12,1], [12,1], [12,1],
-                    [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1]]
-        horaIni = datetime.strptime("11:00", formato)
-        horaFin = datetime.strptime("12:00", formato)
-        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
--- taller3/Taller3Est.py
+++ taller3/Taller3Est.py
@@ -18,10 +18,30 @@ class Estacionamiento:
         return self.__horaMaxReserva
 
     # Algoritmo de Marzullo
-    def verificarDisponibilidad(self, reservas, horaIni, horaFin):
+    def verificarDisponibilidad(self, horaIni, horaFin):
+        reservas = []
+        reservaIni = []
+        reservaFin = []
+        i = 0
+    
+        # Ciclo que escoge los intervalos a considerar en el algoritmo de Marzullo y los
+        # ordena. No toma en cuenta aquellos que estan en la frontera del intervalo de la nueva
+        # reservacion que se quiere agregar.
+        while i < len(self.reservaciones):
+            if (self.reservaciones[i][0] <= horaIni < self.reservaciones[i][1]) or (self.reservaciones[i][0] < horaFin <= self.reservaciones[i][1]):
+                reservaIni.append([self.reservaciones[i][0],-1])
+                reservaFin.append([self.reservaciones[i][1],1])
+            i+=1
+        reservaIni.sort()
+        reservaFin.sort()
+        reservas = reservaIni + reservaFin
+
+        if reservas == []:
+            self.reservaciones.append([horaIni,horaFin])
+            return True
+        
         cont = 0;
         mejor = 0;
-        hayPuesto = True
         mejorInicio = reservas[0]
         mejorFin = reservas[1]
         pos = 2;    
@@ -44,34 +64,17 @@ class Estacionamiento:
                 mejorInicio = mejorFin
                 mejorFin = aux
                 
-            if ((horaFin.hour == mejorInicio[0]) or (horaIni.hour == mejorFin[0])):
-                hayPuesto = True
+            if ((horaFin == mejorInicio[0]) or (horaIni == mejorFin[0])):
+                return True
             else:
-                hayPuesto = False
+                return False
         
-        if hayPuesto:
-            self.reservaciones.append([horaIni.hour,horaFin.hour])
-
-        return hayPuesto        
-    
-# Funcion que escoge los intervalos a considerar en el algoritmo de Marzullo y los
-# ordena. No toma en cuenta aquellos que estan en la frontera del intervalo de la nueva
-# reservacion que se quiere agregar.
-def dividir(seq,horaIni,horaFin):
-    inicio = []
-    fin = []
-    i = 0
-    while i < len(seq):
-        if (seq[i][0] <= horaIni < seq[i][1]) or (seq[i][0] < horaFin <= seq[i][1]):
-            inicio.append([seq[i][0],-1])
-            fin.append([seq[i][1],1])
-        i+=1
-    return inicio,fin
+        self.reservaciones.append([horaIni,horaFin])
+        return True            
     
 if __name__ == '__main__':
     est = Estacionamiento()
     formato = '%H:%M'
-    reservas = []
     
     while True:    
         while True:
@@ -94,19 +97,10 @@ if __name__ == '__main__':
             est.reservaciones.append([horaIni.hour,horaFin.hour])
             print("Se realizo una reservacion exitosa")
         else:
-            ri,rf = dividir(reservas)
-            ri.sort()
-            rf.sort()
-            reservas = ri + rf
-            
-            if reservas != []: 
-                if (est.verificarDisponibilidad(reservas, horaIni, horaFin)):
-                    print("Se realizo una reservacion exitosa")
-                else:
-                    print("No se puede realizar la reserva solicitada")
-            else:
-                est.reservaciones.append([horaIni.hour,horaFin.hour])
+            if (est.verificarDisponibilidad(horaIni.hour, horaFin.hour)):
                 print("Se realizo una reservacion exitosa")
+            else:
+                print("No se puede realizar la reserva solicitada")
 
         while True:
             continuar = raw_input("Desea hacer otra reservacion? (si/no): ")
