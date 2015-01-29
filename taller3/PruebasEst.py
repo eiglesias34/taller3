18dd44be02436b48c07187a252b59081a0dce248
Enrique <eiglesias34@gmail.com>
2015-01-29 16:14:03 -0430

    Paso 4, Caso de prueba erroneo

--- taller3/PruebasEst.py
+++ taller3/PruebasEst.py
@@ -21,3 +21,38 @@ class TestEst(unittest.TestCase):
         horaIni = datetime.strptime("06:00", formato)
         horaFin = datetime.strptime("09:00", formato)
         self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
+        
+        #Esquina: Tratar de realizar una reserva donde el intervalo esta lleno
+        reservas =[[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],
+                   [9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],
+                   [9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],
+                   [10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1]]
+        horaIni = datetime.strptime("07:00", formato)
+        horaFin = datetime.strptime("18:00", formato)
+        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
+        
+        #Esquina: Tratar de realizar una reserva donde el intervalo esta lleno
+        reservas =[[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],[6,-1],
+                   [9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],[9,-1],
+                   [9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],[9,1],
+                   [10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1],[10,1]]
+        horaIni = datetime.strptime("08:00", formato)
+        horaFin = datetime.strptime("10:00", formato)
+        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
+
+        #Frontera: Tratar de realizar una reserva teniendo el estacionamiento
+        #          lleno.
+        reservas = [[6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1], [6,-1],
+                    [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1], [18,1]]
+        horaIni = datetime.strptime("06:00", formato)
+        horaFin = datetime.strptime("18:00", formato)
+        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), False)
+        
+        #Esquina: Ingresar reservacion hora minima despues de varias reservaciones
+        #         que terminen en hora maxima (Falla)
+        reservas =[[6,-1],[7,-1],[8,-1],[9,-1],[10,-1],[11,-1],[12,-1],[13,-1],[14,-1],[15,-1],
+                   [9,1],[10,1],[11,1],[12,1],[13,1],[14,1],[15,1],[16,1],[17,1],[18,1]]
+        horaIni = datetime.strptime("06:00", formato)
+        horaFin = datetime.strptime("09:00", formato)
+        self.assertEqual(est.verificarDisponibilidad(reservas,horaIni,horaFin), True)
+        
